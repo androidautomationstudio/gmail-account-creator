6b64cd1e1e6b5ee2b70f9165b81071c21299b4a5
androidautomationstudio <107746573+androidautomationstudio@users.noreply.github.com>
2023-02-16 17:56:16 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+Gmail account creator // Youtube account creator
+
+The software creates gmail accounts and drops a cookie as soon as the account is created.. You can use the accounts without logging in by using cookies. If cookies are expired you can login to the account as all information for that account are saved ti a text file.
+
+Price is 100$ for a lifetime license (All future updates are free!)
+
+Take a look how the software works : https://youtu.be/wswUDe4VzJM
+
+Telegram : @camoraa
+
+sQwIGq6
